4b731b317ce0b0d2096ac3989fc1aec9373b3e95
mrodz <79176075+mrodz@users.noreply.github.com>
2022-12-11 21:59:34 -0800

    remove files

--- twosum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        looking_for = {}
-        for i, n in enumerate(nums):
-            if n in looking_for:
-                return looking_for[n], i
-            looking_for[target - n] = i
